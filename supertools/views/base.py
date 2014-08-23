0153641e837a39e96070e55e4846bf8b432b9ed9
Andrey Antukh <niwi@niwi.be>
2014-08-23 12:58:23 +0200

    Update views/base.py module with updated version of generic view.

--- supertools/views/base.py
+++ supertools/views/base.py
@@ -1,16 +1,32 @@
-# -*- coding: utf-8 -*-
-
+from django.core.urlresolvers import reverse
 from django.views.generic import View
-from django.template import RequestContext, loader
+from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.shortcuts import render_to_response
 
 from .. import http
+from .. import exceptions as exc
 
 
 class GenericView(View):
     response_cls = http.HttpResponse
     content_type = "text/html"
+    permissions = ()
 
-    def init(self, *args, **kwargs):
+    def handle_exception(self, exception):
+        """
+        Ad-hoc exception handling for all derived views.
+        """
+        ctx = {"exception": exception}
+
+        if isinstance(exception, exc.RedirectRequired):
+            return self.redirect(url=exception.detail)
+        elif isinstance(exception, exc.IntegrityError):
+            return http.HttpConflict(exception)
+
+        return exception
+
+    def init(self, request, *args, **kwargs):
         pass
 
     def get_context_data(self):
@@ -18,12 +34,40 @@ class GenericView(View):
         context.update(self.kwargs)
         return context
 
+    def __handle_permissions(self):
+        for fn in self.permissions:
+            result = fn(self)
+            if isinstance(result, http.HttpResponse):
+                return result
+            elif result == False:
+                raise exc.PermissionDenied("Forbidden")
+
+        if hasattr(self, "handle_permissions"):
+            return self.handle_permissions()
+
     def dispatch(self, request, *args, **kwargs):
-        self.init(request, *args, **kwargs)
-        return super(GenericView, self).dispatch(request, *args, **kwargs)
+        try:
+            self.init(request, *args, **kwargs)
+            result = self.__handle_permissions()
+            if isinstance(result, http.HttpResponse):
+                return result
+            return super().dispatch(request, *args, **kwargs)
+        except Exception as e:
+            response = self.handle_exception(e)
+            if isinstance(response, Exception):
+                raise
+            return response
+
+    def redirect(self, reverseurl=None, *, url=None, args=None, kwargs=None):
+        """
+        Simple redirect helper.
+        """
+        if not url:
+            url = reverse(reverseurl, args=args, kwargs=kwargs)
+        return http.HttpRedirect(url)
 
     def render(self, template=None, context=None, data=None,
-               response_cls=None, content_type=None):
+               response_cls=None, content_type=None, status_code=None):
 
         output_data = data or b""
 
@@ -32,9 +76,8 @@ class GenericView(View):
             _context.update(context or {})
 
             context_instance = RequestContext(self.request)
-            output_data = loader.render_to_string(
-                                template, _context,
-                                context_instance=context_instance)
+            output_data = render_to_string(template, _context,
+                                           context_instance=context_instance)
 
         if content_type is None:
             content_type = self.content_type
@@ -42,7 +85,11 @@ class GenericView(View):
         if not response_cls:
             response_cls = self.response_cls
 
-        return response_cls(output_data, content_type=content_type)
+        response = response_cls(output_data, content_type=content_type)
+        if status_code:
+            response.status_code = status_code
+
+        return response
 
 
 class GenericTemplateView(GenericView):
