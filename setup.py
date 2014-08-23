ec84c3d63baefc3a0791464cf73de3b997c8aa1e
Andrey Antukh <niwi@niwi.be>
2014-08-23 13:01:35 +0200

    Increment version to 0.3.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-supertools',
-    version = "0.2",
+    version = "0.3",
     description = "Generic tools for django apps.",
     long_description = "",
     keywords = 'django',
